6d707e0feda940effab23f694aea71822dd53e0c
Harshal Gunda <harshal.gunda@cuelogic.com>
2019-06-30 10:32:04 +0530

    onload todo page data gets retrieved for that user only

--- ToDo Assignment/js/todo.js
+++ ToDo Assignment/js/todo.js
@@ -12,7 +12,7 @@ function add_todo_item()
 	let myInput = document.getElementById("myInput").value;
 	let sdate = document.getElementById("sdate").value;
 	let ddate = document.getElementById("ddate").value;
-	let ispublictrue = document.getElementById("ispublictrue").value;
+	let ispublictrue = document.getElementById("ispublictrue").checked.value;
 	let categories = document.querySelector('input[name="categories"]:checked').value;
 	let desc = document.getElementById("description").value;
 
@@ -34,7 +34,9 @@ function add_todo_item()
 	let arr_length = (code_todo_array.length)-1;
 	
 	let new_row = document.createElement("tr");
-	new_row.innerHTML = "<td>" + code_todo_array[arr_length].todo_name + "</td>" + 
+
+	new_row.innerHTML = "<td>" + "<input type='checkbox'id='delete' value='yes'" + "</td>" + 
+						"<td>" + code_todo_array[arr_length].todo_name + "</td>" + 
 						"<td>" + code_todo_array[arr_length].start_date + "</td>" +
 						"<td>" + code_todo_array[arr_length].end_date + "</td>" +
 						"<td>" + code_todo_array[arr_length].is_public + "</td>" +
@@ -44,4 +46,26 @@ function add_todo_item()
 	document.getElementById("todo_table").appendChild(new_row);
 
 	/* window.location.reload(); */
+}
+
+function show_users_todo_on_page_load()
+{
+	let user_id =  sessionStorage.getItem("logged_in_user");
+	let code_array = JSON.parse(localStorage.getItem("local_storage_array"));
+	let code_todo_array = code_array[user_id].to_do_user;
+
+	for(let i=0; i<(code_todo_array.length); i++)
+	{
+		let new_row = document.createElement("tr");
+
+		new_row.innerHTML = "<td>" + "<input type='checkbox'id='delete' value='yes'" + "</td>" + 
+							"<td>" + code_todo_array[i].todo_name + "</td>" + 
+							"<td>" + code_todo_array[i].start_date + "</td>" +
+							"<td>" + code_todo_array[i].end_date + "</td>" +
+							"<td>" + code_todo_array[i].is_public + "</td>" +
+							"<td>" + code_todo_array[i].categories + "</td>" +
+							"<td>" + code_todo_array[i].description + "</td>" ;
+
+		document.getElementById("todo_table").appendChild(new_row);
+	}
 }
